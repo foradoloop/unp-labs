b31a81827b81730182d76fdd04bb716edd18c536
foradoloop <255560900+foradoloop@users.noreply.github.com>
2026-01-23 09:14:57 -0300

    Add iterative TCP echo server

---
+++ tcpcliserv/echo/echoserv01.c
@@ -0,0 +1,42 @@
+#include "unp-labs.h"
+
+#define SERV_PORT 8080
+#define LISTENQ 5
+#define MAXLINE 100
+
+int main(void)
+{
+	struct sockaddr_in servaddr;
+
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	int listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+
+	Bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	Listen(listenfd, LISTENQ);
+
+	for (;;) {
+		struct sockaddr_in cliaddr;
+
+		bzero(&cliaddr, sizeof(cliaddr));
+		socklen_t clilen = sizeof(cliaddr);
+		int connfd = Accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+
+		for (;;) {
+			char buffer[MAXLINE];
+			ssize_t nread = Read(connfd, buffer, MAXLINE);
+
+			if (nread <= 0) {
+				Close(connfd);
+				break;
+			}
+			Writen(connfd, buffer, nread);
+		}
+	}
+
+	return 0;
+}
